edcc5a4d275c3c47cee6725550f396558362b483
Christos Stavrakakis <cstavr@grnet.gr>
2013-01-24 17:21:51 +0200

    Take build area as arguments

--- ci/autopkg_debian.sh
+++ ci/autopkg_debian.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env sh
 
-TEMP=$(mktemp -d /tmp/autopkg_debian_XXXXX)
-
 if [ -z $(git branch -a | grep " debian-develop") ]; then
   git branch debian-develop origin/debian-develop
 fi
 
-devflow-autopkg -b $TEMP snapshot
+devflow-autopkg -b $1 snapshot
