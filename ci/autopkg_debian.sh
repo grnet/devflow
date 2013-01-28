d23bbb48be33a59d8d1d6d420772a7d10bae4062
Christos Stavrakakis <cstavr@grnet.gr>
2013-01-28 14:21:06 +0200

    CI fixes

--- ci/autopkg_debian.sh
+++ ci/autopkg_debian.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env sh
 
-if [ -z $(git branch -a | grep " debian-develop") ]; then
-  git branch debian-develop origin/debian-develop
-fi
-
-devflow-autopkg -b $1 snapshot
+devflow-autopkg -b $1 snapshot $2
