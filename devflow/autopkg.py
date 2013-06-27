d3af231fa7c6ccca9a6e535796a16fe8acd5bd02
Christos Stavrakakis <cstavr@grnet.gr>
2013-06-27 19:56:13 +0300

    Sign tags

--- devflow/autopkg.py
+++ devflow/autopkg.py
@@ -237,10 +237,21 @@ def main():
     # Update the version files
     versioning.update_version()
 
+
+    if not options.sign:
+        sign_tag_opt = None
+    elif options.keyid:
+        sign_tag_opt = "-u=%s" % options.keyid
+    elif mode == "release":
+        sign_tag_opt = "-s"
+    else:
+        sign_tag_opt = None
+
     # Tag branch with python version
     branch_tag = python_version
+    tag_message = "%s version %s" % (mode.capitalize(), python_version)
     try:
-        repo.git.tag(branch_tag, branch)
+        repo.git.tag(branch_tag, branch, sign_tag_opt, "-m=%s" % tag_message)
     except GitCommandError:
         # Tag may already exist, if only the debian branch has changed
         pass
@@ -280,8 +291,9 @@ def main():
     repo.git.commit("-s", "-a", m="Bump version to %s" % debian_version)
     # Tag debian branch
     debian_branch_tag = "debian/" + utils.version_to_tag(debian_version)
+    tag_message = "%s version %s" % (mode.capitalize(), debian_version)
     if mode == "release":
-        repo.git.tag(debian_branch_tag)
+        repo.git.tag(debian_branch_tag, sign_tag_opt, "-m=%s" % tag_message)
 
     # Add version.py files to repo
     call("grep \"__version_vcs\" -r . -l -I | xargs git add -f")
