9db4ea0bc9173701d9263adb2f2548ff403deea2
Christos Stavrakakis <cstavr@grnet.gr>
2014-11-06 09:36:52 +0000

    Update GitPython dependency

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ CLASSIFIERS = []
 
 # Package requirements
 INSTALL_REQUIRES = [
-    'gitpython', 'sh', 'configobj', 'ansicolors'
+    'gitpython>=0.3.2RC1', 'sh', 'configobj', 'ansicolors'
 ]
 
 # Provided as an attribute, so you can append to these instead
